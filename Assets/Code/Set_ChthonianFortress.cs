33cb45b905a8fa5d6d1e2a06c22c8b4b361d645e
Tatu Koskela <tatkos@utu.fi>
2021-04-26 21:34:35 +0300

    Created fortress

---
+++ Assets/Code/Set_ChthonianFortress.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code
+{
+    public class Set_ChthonianCitadel : Settlement
+    {
+        public Set_ChthonianCitadel(Location loc) : base(loc)
+        {
+            this.isHuman = false;
+            name = "Chthonian Citadel";
+
+            defensiveStrengthMax = 20;
+            militaryCapAdd += loc.map.param.chthonian_armyStrength;
+            militaryRegenAdd = 1;
+
+        }
+
+        public override Sprite getCustomTerrain(Hex hex)
+        {
+            int c = hex.graphicalIndexer % 2;
+            if (c == 0)
+            {
+                return World.staticMap.world.textureStore.hex_special_flesh; //TODO change sprite
+            }
+            else
+            {
+                return World.staticMap.world.textureStore.hex_special_flesh2; //TODO change sprite
+            }
+        }
+
+        public override void turnTick()
+        {
+            base.turnTick();
+
+            foreach (Hex h in location.territory)
+            {
+                h.flora = null;
+            }
+
+            //Create Infiltrator from the Citadel if one doesn't exist.
+            if (checkIfLinkedAgentIsAlive() == false)
+            {
+                // Debug.Log("Trying to create infiltrator...");
+                SpawnInfiltrator();
+            }
+
+        }
+
+        public override Sprite getSprite()
+        {
+            return location.map.world.textureStore.loc_flesh; //TODO change sprite
+        }
+
+        public override void checkUnitSpawning()
+        {
+            spawnCounter += 1;
+            if (spawnCounter > 5)
+            {
+                spawnCounter = 0;
+
+                if (this.attachedUnit != null) { throw new Exception(); }
+
+                if (location.soc is SG_Chthonians == false) { return; }
+                Unit_ChthonianArmy army = new Unit_ChthonianArmy(location, (SG_Chthonians)location.soc);
+                location.map.units.Add(army);
+                army.maxHp = (int)this.getMilitaryCap();
+                this.attachedUnit = army;
+                World.log("Created new Chthonian Army");
+            }
+        }
+    }
+}
